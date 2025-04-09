3c3dbfc73fe91c6abd54fc9d741bb32ffb368a8b
davydenko919 <davydenko919@gmail.com>
2025-04-09 14:45:02 +0300

    s1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import PinoHttp from 'pino-http';
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(PinoHttp({
+  transport:{
+    target: "pino-pretty"
+  }
+}))
 
 app.get('/', (req, res) => {
   // тіло функції-обробника
@@ -10,4 +17,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
-}); 
+});
