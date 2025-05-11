6fb19a63379d0374bbad98fcfc512850a59182ca
davydenko919 <davydenko919@gmail.com>
2025-05-12 00:57:06 +0300

    s111

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import tripRoutes from './routes/tripsRoutes.js';
 
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import authRoutes from './routes/auth.js';
 
 const app = express();
 
-
-
+app.use('/auth', authRoutes);
 app.use('/trips', tripRoutes);
 
 app.use(notFoundHandler);
