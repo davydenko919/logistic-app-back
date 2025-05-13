f093c12546ab22405ec21131611985a249a1a933
davydenko919 <davydenko919@gmail.com>
2025-05-14 00:36:21 +0300

    sadsadsa

--- src/app.js
+++ src/app.js
@@ -6,13 +6,15 @@ import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import authRoutes from './routes/auth.js';
+import { auth } from './middlewares/auth.js';
+
 
 const app = express();
 
 app.use(cookieParser());
 
 app.use('/auth', authRoutes);
-app.use('/trips', tripRoutes);
+app.use('/trips', auth, tripRoutes);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
