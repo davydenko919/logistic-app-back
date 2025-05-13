6717bb292ba657d55cd557021c4715c81567be0c
davydenko919 <davydenko919@gmail.com>
2025-05-13 14:48:10 +0300

    ssssss

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 
 import tripRoutes from './routes/tripsRoutes.js';
+import cookieParser from 'cookie-parser';
 
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import authRoutes from './routes/auth.js';
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use('/auth', authRoutes);
 app.use('/trips', tripRoutes);
