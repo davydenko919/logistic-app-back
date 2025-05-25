2e1d03d34268e8f35a89f3e5e7220a8f7a88ef00
davydenko919 <davydenko919@gmail.com>
2025-05-25 15:08:50 +0300

    asdasdsa

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import tripRoutes from './routes/tripsRoutes.js';
 import adminTrips from './routes/adminTrips.js';
 import usersRoutes from './routes/usersRoutes.js';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import authRoutes from './routes/auth.js';
@@ -13,8 +13,12 @@ import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const app = express();
 
+app.use(cors());
+
 app.use('/api-docs', swaggerDocs());
 app.use(cookieParser());
+
+
 
 app.use('/auth', authRoutes);
 app.use('/trips', auth, tripRoutes);
